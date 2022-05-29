b23c3890b81614d2ab04838bcc857f3fad59a924
shine-jayakumar <shinejayakumar@yahoo.com>
2022-05-29 14:47:04 +0530

    Moved connect uri to constants.py

--- constants.py
+++ constants.py
@@ -2,7 +2,9 @@
 # Contains contstants
 
 from typing import NewType
+import os
 
 table = NewType('DBTable', str)
 dbconnection = NewType('DBConnection', str)
 
+SQLALCHEMY_CONNECT_URI = os.environ.get('SQLALCHEMY_MYSQL_URI')
--- persistent.py
+++ persistent.py
@@ -21,7 +21,7 @@ class Record:
         # use tablename if specified or generate one
         self.tablename:str = tablename or self.record_id
         self.table_is_valid = False
-        self.connect_uri:str = os.environ.get('SQLALCHEMY_MYSQL_URI')
+        self.connect_uri:str = os.environ.get(SQLALCHEMY_CONNECT_URI)
         self.engine:str = ''
         self.conn:str = ''
         self.meta:str = ''
